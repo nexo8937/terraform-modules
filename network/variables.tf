a3091871b2f3dacb8c29a643dbed73802c891f35
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2024-04-14 11:37:07 +0000

    network module

--- network/variables.tf
+++ network/variables.tf
@@ -1,23 +1,21 @@
 variable "app" {
     description = "Application Name"
-#    default = "Brain-Scale"
+    default = 'application"
 }
 
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
-#  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_ciders" {
-#   default = [
-#      "10.0.11.0/24",
-#      "10.0.12.0/24"
-#  ]
+   default = [
+      "10.0.11.0/24"
+  ]
 }
 
 variable "private_subnet_ciders" {
-#   default = [
-#      "10.0.21.0/24",
-#      "10.0.22.0/24"
-#  ]
+   default = [
+      "10.0.21.0/24"
+  ]
 }
