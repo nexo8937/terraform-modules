99f0dd785ad5cb1024c131e6cfa5fe60035d805d
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2024-04-14 12:16:25 +0000

    creating ecr module

---
+++ ecr/ecr.tf
@@ -0,0 +1,45 @@
+#ECR & Permissions
+
+#----------------ECR----------------
+
+resource "aws_ecr_repository" "ecr-repo" {
+  count                = length(var.ecr-repo-name) 
+  name                 = var.ecr-repo-name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
+#----------------Role & Policy----------------
+
+#EC2 Role
+resource "aws_iam_role" "ecr_role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+#Attach Policy to Role
+resource "aws_iam_policy_attachment" "ecr_policy_attachment" {
+  name       = "AmazonEC2ContainerRegistryFullAccess"
+  roles      = [aws_iam_role.ecr_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+#EC2 Instance Profile
+resource "aws_iam_instance_profile" "ecr_instance_profile" {
+  name = "${var.app}-ecr-instance-profile"
+  role = aws_iam_role.ecr_role.name
+}
---
+++ ecr/variables.tf
@@ -0,0 +1,11 @@
+variable "ecr-repo-name" {
+  description = "The name of ecr repository"
+  default     = [
+    "application"
+    ]
+}
+
+variable "app" {
+    description = "Application Name"
+    default = "application"
+}
