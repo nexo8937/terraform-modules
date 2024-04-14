f5f033e84d7b4b3795fd57c628a4efaa199fc58a
Ubuntu <ubuntu@ip-172-31-88-68.ec2.internal>
2024-04-14 11:12:29 +0000

    changing variables

--- network/variables.tf
+++ network/variables.tf
@@ -1,28 +1,23 @@
-variable "region" {
-    description = "AWS region"
-    default = "us-east-1"
-}
-
 variable "app" {
     description = "Application Name"
-    default = "Brain-Scale"
+#    default = "Brain-Scale"
 }
 
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+#  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_ciders" {
-   default = [
-      "10.0.11.0/24",
-      "10.0.12.0/24"
-  ]
+#   default = [
+#      "10.0.11.0/24",
+#      "10.0.12.0/24"
+#  ]
 }
 
 variable "private_subnet_ciders" {
-   default = [
-      "10.0.21.0/24",
-      "10.0.22.0/24"
-  ]
+#   default = [
+#      "10.0.21.0/24",
+#      "10.0.22.0/24"
+#  ]
 }
