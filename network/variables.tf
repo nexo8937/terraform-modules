835d1f3a7fb5f32421b9c4c3e97b484d83051a61
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-04-20 11:55:46 +0400

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,21 +1,21 @@
 variable "app" {
     description = "Application Name"
-    default = "application"
+    default = ""
 }
 
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  default     = ""
 }
 
 variable "public_subnet_ciders" {
    default = [
-      "10.0.11.0/24"
+      ""
   ]
 }
 
 variable "private_subnet_ciders" {
    default = [
-      "10.0.21.0/24"
+      ""
   ]
 }
