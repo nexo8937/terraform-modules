b8e8ff5793ce6dcec653e343090c398c2823e08b
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-04-20 13:46:05 +0400

    Update outputs.tf

--- loadbalancer/outputs.tf
+++ loadbalancer/outputs.tf
@@ -2,3 +2,8 @@ output "load-balancer-dns-name" {
   description = "DNS name of the load balancer"
   value = aws_lb.load-balancer.dns_name
 }
+
+output "load-balancer-target-group" {
+  description = "target group of the load balancer"
+  value = aws_lb_target_group.target-group.arn
+}
