3db371cc876d4ba1b49f81dcc588b88107f4ebdb
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-19 15:32:53 +0400

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,5 +1,4 @@
 #EKS Role
-
 resource "aws_iam_role" "cluster_role" {
   name               = "${var.app}-cluster-role"
   assume_role_policy = <<EOF
@@ -18,6 +17,7 @@ resource "aws_iam_role" "cluster_role" {
 EOF
 }
 
+#EKS Policy attachment
 resource "aws_iam_role_policy_attachment" "attach_eks_cluster_policy" {
   role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -25,7 +25,6 @@ resource "aws_iam_role_policy_attachment" "attach_eks_cluster_policy" {
 
 
 #EKS Cluster
-
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "${var.app}-cluster"
   role_arn = aws_iam_role.cluster_role.arn
@@ -37,5 +36,82 @@ resource "aws_eks_cluster" "eks_cluster" {
 #    security_group_ids = ["sg-12345678"]  
     endpoint_public_access = true
   }
+
+  depends_on = [aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy]
+
+}
+
+#Addons
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  addon_name   = "coredns"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  addon_name   = "vpc-cni"
+}
+
+#NodeGroup
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+
+#NodeGroup
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "private-nodes"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = var.private_subnets
+  capacity_type   = "ON_DEMAND"
+  instance_types  = var.instance_type
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size 
+    min_size     = var.min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
