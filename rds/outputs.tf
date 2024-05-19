ed3f319db221b5fed1531eebab27f771b6bfb78a
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-19 17:40:20 +0400

    Update outputs.tf

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,10 +1,10 @@
 output "rds_endpoint" {
   value = aws_db_instance.db.endpoint
 }
-output "db-sg" {
+output "rds-access" {
   value = aws_security_group.rds.id
 }
 
-output "rds-access" {
-  value = aws_security_group.rds-access.id
-}
+#output "rds-access" {
+#  value = aws_security_group.rds-access.id
+#}
