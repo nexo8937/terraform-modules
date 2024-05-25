be84ae168f9d2cdb0bd311955534be8ca1aa8da8
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-25 17:21:46 +0400

    Update outputs.tf

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,7 +1,1 @@
-output "eks_cluster_certificate_authority_data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority
-}
 
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
