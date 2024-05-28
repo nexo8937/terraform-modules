6b28e063286d742440b84b0efae093c72812cc4d
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-28 18:45:19 +0400

    Update outputs.tf

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -2,3 +2,18 @@ output "cluster_name" {
   description = "Name of the EKS cluster"
   value       = aws_eks_cluster.eks_cluster.name
 }
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "The certificate authority data for the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "token" {
+  description = "The authentication token for the EKS cluster."
+  value       = aws_eks_cluster_auth.eks_cluster.token
+}
