2dc05d82aa99e9488d296011ac2017ee81ecf539
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-28 13:48:13 +0400

    Update helm.tf

--- helm/helm.tf
+++ helm/helm.tf
@@ -1,11 +1,11 @@
 resource "helm_release" "my_application" {
   name       = "my-release-name"
-  chart      = "../helm"  # Path to your local Helm chart
+  chart      = "../../helm"  # Path to your local Helm chart
   namespace  = "default"  # Kubernetes namespace to deploy to
 
   # Optionally, specify values
 values = [
-  file("../helm/values_dev.yaml")
+  file("../../helm/values_dev.yaml")
 ]
   # Or directly set values
   set {
